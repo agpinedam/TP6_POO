71f223c2f3ea75f9d3b46aca884682f809e759f1
Angie <agpinedam@unal.edu.co>
2024-01-29 14:25:37 +0100

    Verification existance of table

--- app/src/main/java/tp6bis/EntityManagerImpl.java
+++ app/src/main/java/tp6bis/EntityManagerImpl.java
@@ -3,19 +3,25 @@ package tp6bis;
 import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class DatabaseManager {
+public class EntityManagerImpl {
 
-    // Database configuration
     private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/GolfClub";
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
 
     // Method to create a table in PostgreSQL based on an object of a class
-    public static void createTableFromClass(Object object) {
+    public static void persistance(Object object) {
         // Get the class name
         String className = object.getClass().getSimpleName();
+
+        // Check if the table already exists
+        if (tableExists(className)) {
+            System.out.println("Table already exists.");
+            return;
+        }
 
         // Get the fields of the class
         Field[] fields = object.getClass().getDeclaredFields();
@@ -27,8 +33,13 @@ public class DatabaseManager {
             String fieldName = field.getName();
             String fieldType = field.getType().getSimpleName();
 
-            // Assume all fields are of primitive type or String in this example
-            sqlQuery.append(fieldName).append(" ").append(getSQLDataType(fieldType)).append(", ");
+            // Adjust the id field to be serial (autoincrement)
+            if (fieldName.equals("id")) {
+                sqlQuery.append(fieldName).append(" SERIAL, ");
+            } else {
+                // Assume all other fields are of primitive type or String in this example
+                sqlQuery.append(fieldName).append(" ").append(getSQLDataType(fieldType)).append(", ");
+            }
         }
 
         // Remove the extra comma at the end and close the query
@@ -41,6 +52,19 @@ public class DatabaseManager {
             System.out.println("Table created successfully.");
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    // Method to check if a table exists in the database
+    private static boolean tableExists(String tableName) {
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+            Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT to_regclass('" + tableName + "')");
+            resultSet.next();
+            return resultSet.getString(1) != null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
         }
     }
 
@@ -66,6 +90,6 @@ public class DatabaseManager {
 
     public static void main(String[] args) {
         Club clubExample = new Club();
-        createTableFromClass(clubExample);
+        persistance(clubExample);
     }
 }
